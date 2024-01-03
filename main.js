bcd0b712c285a2f108b5683aed8b987a5ee4eaa2
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-01-03 01:11:15 -0300

    updated project

--- main.js
+++ main.js
@@ -1,67 +1,31 @@
 const form = document.getElementById("form-atividade");
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
-const notaMinima = Number(prompt("Digite a nota mínima: "));
-const atividades = [];
-const notas = [];
 let linha;
+let linhas = "";
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     adicionaLinha();
     atualizaTabela();
-    atualizaMediaFinal();
 });
 
 function adicionaLinha() {
-    const inputNomeAtividade = document.getElementById('nome-atividade');
-    const inputNotaAtividade = document.getElementById('nota-atividade');
-
-    if (atividades.includes(inputNomeAtividade.value)) {
-        alert(`A atividade '${inputNomeAtividade.value}' já foi inserida!`);
-        linha = "";
-    } else {
-        atividades.push(inputNomeAtividade.value);
-        notas.push(Number(inputNotaAtividade.value));
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
         
-        linha = '<tr>';
-        linha += `<td>${inputNomeAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value}</td>`;
-        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
-        linha += '</tr>';
-    }
+    linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
 
-    inputNomeAtividade.value = "";
-    inputNotaAtividade.value = "";
+    inputNomeContato.value = "";
+    inputNumeroContato.value = "";
 }
 
-
-
 function atualizaTabela() {
-    let linhas = ""; /* poderia colocar essa variável no escopo global e toda vez que o evento submit fosse chamado seria acrescentado a nova linha com o valor antigo do 
-    submit anterior... */
-
     const corpoTabela = document.querySelector("tbody");
-    linhas = corpoTabela.innerHTML;
     linhas += linha;
 
     corpoTabela.innerHTML = linhas;
-}
-
-function atualizaMediaFinal() {
-    const mediaFinal = calculaMediaFinal();
-
-    document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(1);
-    const mediaFinalResultado = document.getElementById("media-final-resultado");
-    mediaFinalResultado.innerHTML = mediaFinal >= notaMinima ? '<span class="result aprovado">Aprovado</span>' : '<span class="result reprovado">Reprovado</span>';
-}   
-
-function calculaMediaFinal() {
-    let somaDasNotas = 0;
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i];
-    }
-
-    return somaDasNotas / notas.length;
+    linha = "";
 }
